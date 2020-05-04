635c92fe4cd8952599f0669cff5299ad3b7aae77
NamNS <apocalypsetank234@gmail.com>
2020-05-04 13:23:39 +0700

    NamNS: implement ProductEntity classes

---
+++ src/main/java/com/company/businessprocess/product/ProductService.java
@@ -0,0 +1,16 @@
+package com.company.businessprocess.product;
+
+import com.company.businessprocess.dto.ProductResponse;
+import com.company.businessprocess.entity.ProductEntity;
+
+import java.util.Collection;
+
+public interface ProductService {
+    Collection<ProductResponse> getAllProduct();
+
+    ProductEntity addProduct(ProductEntity newEntity);
+
+    ProductEntity updateProduct(Integer id, ProductEntity updateEntity);
+
+    void deleteProduct(Integer id);
+}
